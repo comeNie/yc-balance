139e6410dca5a0b03612113f349fbc2911599e14
liquid1993 <1522443789@qq.com>
2016-11-25 14:41:44 +0800

    docker地址修改为10.19.13.20

--- onekey-docker.sh
+++ onekey-docker.sh
@@ -3,10 +3,10 @@ git reset --hard origin/master
 git pull 
 chmod a+x onekey-docker.sh 
 gradle clean && gradle build -x test 
-docker build -t 10.19.13.18:5000/yc-balance:${tagversion} .
-docker push 10.19.13.18:5000/yc-balance:${tagversion}
+docker build -t 10.19.13.20:5000/yc-balance:${tagversion} .
+docker push 10.19.13.20:5000/yc-balance:${tagversion}
 
 docker rmi aioptapp/yc-balance:${tagversion}
-docker tag 10.19.13.18:5000/yc-balance:${tagversion} aioptapp/yc-balance:${tagversion}
+docker tag 10.19.13.20:5000/yc-balance:${tagversion} aioptapp/yc-balance:${tagversion}
 docker login --username=aioptapp --password=aioptapp@123 --email=wuzhen3@asiainfo.com 
 docker push aioptapp/yc-balance:${tagversion}
