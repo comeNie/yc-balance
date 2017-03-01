040d8c322def249e87d0c8d1b3a4219a30488f84
liquid1993 <1522443789@qq.com>
2017-03-01 15:29:22 +0800

    账单表初始化

---
+++ balance-core/src/main/java/com/ai/slp/balance/dao/mapper/bo/TAccountParam.java
@@ -0,0 +1,146 @@
+package com.ai.slp.balance.dao.mapper.bo;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+public class TAccountParam {
+    private String id;
+
+    private String targetType;
+
+    private String targetId;
+
+    private String targetName;
+
+    private String accountCls;
+
+    private Integer creditlimit;
+
+    private BigDecimal discount;
+
+    private String accountType;
+
+    private String accountPeriod;
+
+    private String accountDay;
+
+    private String content;
+
+    private Timestamp createTime;
+
+    private Timestamp modifyTime;
+
+    private String status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getTargetType() {
+        return targetType;
+    }
+
+    public void setTargetType(String targetType) {
+        this.targetType = targetType == null ? null : targetType.trim();
+    }
+
+    public String getTargetId() {
+        return targetId;
+    }
+
+    public void setTargetId(String targetId) {
+        this.targetId = targetId == null ? null : targetId.trim();
+    }
+
+    public String getTargetName() {
+        return targetName;
+    }
+
+    public void setTargetName(String targetName) {
+        this.targetName = targetName == null ? null : targetName.trim();
+    }
+
+    public String getAccountCls() {
+        return accountCls;
+    }
+
+    public void setAccountCls(String accountCls) {
+        this.accountCls = accountCls == null ? null : accountCls.trim();
+    }
+
+    public Integer getCreditlimit() {
+        return creditlimit;
+    }
+
+    public void setCreditlimit(Integer creditlimit) {
+        this.creditlimit = creditlimit;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType == null ? null : accountType.trim();
+    }
+
+    public String getAccountPeriod() {
+        return accountPeriod;
+    }
+
+    public void setAccountPeriod(String accountPeriod) {
+        this.accountPeriod = accountPeriod == null ? null : accountPeriod.trim();
+    }
+
+    public String getAccountDay() {
+        return accountDay;
+    }
+
+    public void setAccountDay(String accountDay) {
+        this.accountDay = accountDay == null ? null : accountDay.trim();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    public Timestamp getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Timestamp modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+}
---
+++ balance-core/src/test/java/com/ai/slp/balance/test/api/translatorbill/impl/tranlatorBillSVImplTest.java
@@ -0,0 +1,58 @@
+package com.ai.slp.balance.test.api.translatorbill.impl;
+
+import com.ai.opt.base.exception.BusinessException;
+import com.ai.opt.sdk.util.CollectionUtil;
+import com.ai.opt.sdk.util.DateUtil;
+import com.ai.opt.sdk.util.UUIDUtil;
+import com.ai.slp.balance.api.accountmaintain.interfaces.IAccountMaintainSV;
+import com.ai.slp.balance.api.accountmaintain.param.AccountUpdateParam;
+import com.ai.slp.balance.api.accountmaintain.param.RegAccReq;
+import com.ai.slp.balance.api.accountquery.interfaces.IAccountQuerySV;
+import com.ai.slp.balance.api.accountquery.param.AccountIdParam;
+import com.ai.slp.balance.api.accountquery.param.AccountInfoVo;
+import com.ai.slp.balance.api.accountquery.param.CustIdParam;
+import com.ai.slp.balance.api.translatorbill.interfaces.IBillGenerateSV;
+import com.ai.slp.balance.service.business.interfaces.IBillGenerateBusiSV;
+import com.alibaba.fastjson.JSON;
+import junit.framework.TestCase;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.appender.SyslogAppender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "/context/core-context.xml" })
+public class tranlatorBillSVImplTest extends TestCase {
+
+    private static final Logger log = LogManager.getLogger(tranlatorBillSVImplTest.class);
+
+
+    //账单生成
+    @Autowired
+    private IBillGenerateSV billGenerateSV;
+
+
+
+    @Test
+    public void testBillGenerate()throws Exception{
+        try {
+
+            boolean b = billGenerateSV.generateBill("2");
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("出错：" + e.getMessage());
+        }
+    }
+
+
+    @Test
+    public void test
+
+
+}
