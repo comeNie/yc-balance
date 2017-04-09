138630a6d6b353ed94349941e5770a990db57999
shancc <shancc@lenovo-pc>
2017-04-09 19:08:23 +0800

    未使用列表不查询已过期的数量

--- balance-core/src/main/java/com/ai/slp/balance/service/atom/impl/FunDiscountCouponAtomSVImpl.java
+++ balance-core/src/main/java/com/ai/slp/balance/service/atom/impl/FunDiscountCouponAtomSVImpl.java
@@ -203,6 +203,7 @@ public class FunDiscountCouponAtomSVImpl implements IDiscountCouponAtomSV {
 		FunDiscountCouponCriteria.Criteria critreia = funDiscountCouponCriteria.createCriteria();
 		critreia.andStatusEqualTo(request.getStatus());
 		critreia.andUserIdEqualTo(request.getUserId());
+		critreia.andEffectiveEndTimeLessThanOrEqualTo(DateUtil.getSysDate());
 		FunDiscountCouponMapper mapper = MapperFactory.getFunDiscountCouponMapper();
 		int countByExample = mapper.countByExample(funDiscountCouponCriteria);
 		return countByExample;
