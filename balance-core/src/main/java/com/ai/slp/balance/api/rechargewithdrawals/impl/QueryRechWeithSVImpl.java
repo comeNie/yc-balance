c95482cdc555c13e08367b4d4c67b4841b001a64
shancc <shancc@lenovo-pc>
2017-05-24 16:34:44 +0800

    scc

--- balance-core/src/main/java/com/ai/slp/balance/api/rechargewithdrawals/impl/QueryRechWeithSVImpl.java
+++ balance-core/src/main/java/com/ai/slp/balance/api/rechargewithdrawals/impl/QueryRechWeithSVImpl.java
@@ -112,6 +112,21 @@ public class QueryRechWeithSVImpl implements IRechargeWithdrawalsSV {
 
 	@Override
 	public QueryInfoResponse queryRechWith(QueryByIdRequest param) throws BusinessException, SystemException {
-		return iQueryRechWeithBusiSV.queryRechWith(param);
+		QueryInfoResponse queryInfoResponse = new QueryInfoResponse();
+		ResponseHeader responseHeader = new ResponseHeader();
+		try {
+			iQueryRechWeithBusiSV.queryRechWith(param);
+            responseHeader.setIsSuccess(true);
+            responseHeader.setResultCode(ExceptCodeConstants.Special.SUCCESS);
+            responseHeader.setResultMessage("充值提现查询成功");
+        }catch (BusinessException businessException){
+            responseHeader.setResultCode(businessException.getErrorCode());
+            responseHeader.setResultMessage(businessException.getErrorMessage());
+        }catch (Exception e){
+        	logger.error(ExceptCodeConstants.Special.SYSTEM_ERROR, e);
+            responseHeader.setResultCode(ExceptCodeConstants.Special.SYSTEM_ERROR);
+            responseHeader.setResultMessage("充值提现查询失败");
+        }
+		return queryInfoResponse;
 	}
 }
